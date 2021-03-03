e5ba532dbbee6bdc466cb1aa5eb50e415eacedd7
Yodagost45 <jonathan78564@gmail.com>
2021-03-03 19:42:43 +0000

    Worked out outline functions for AI

--- Slither Link/JS/slitherLink.js
+++ Slither Link/JS/slitherLink.js
@@ -73,6 +73,165 @@ function displayNum(idOne,idTwo){
     return squares[idOne][idTwo];
   }
 }
+//AI ***********************************************************************
+function complete(){
+  while (!check_solution()){
+    while(move_available()){
+      rule_one();
+      rule_two();
+      rule_three();
+      rule_four();
+    }
+    if (check_solution()){
+      break;
+    }
+    rule_five();
+    if (check_solution()){
+      break;
+    }
+    backtracking_search();
+  }
+  for(i=0; i < 5; i++){
+    for(j=0; j < 5; j++){
+      if (squares[i][j] == 0){
+        //redundant
+        horizontalLines[i][j] = 2;
+        horizontalLines[i+1][j] = 2;
+        verticalLines[i][j] = 2;
+        verticalLines[i][j+1] = 2;
+      }
+      if(squares[i][j] == remove_x(horizontalLines[i][j]) + remove_x(horizontalLines[i+1][j]) + remove_x(verticalLines[i][j]) + remove_x(verticalLines[i][j+1])){
+        //if square has enough lines, make all 0s Xs(2s) (rule_one)
+        if(horizontalLines[i][j] == 0){
+          horizontalLines[i][j] = 2;
+        }
+        if(horizontalLines[i+1][j] == 0){
+          horizontalLines[i+1][j] = 2;
+        }
+        if(verticalLines[i][j] == 0){
+          verticalLines[i][j] = 2;
+        }
+        if(verticalLines[i][j+1] == 0){
+          horizontalLines[i][j+1] = 2;
+        }
+      }
+      //Rule: if one point has two edges connecting, the other two are empty
+      //
+      if(horizontalLines[i][j] == 1 && verticalLines[i][j] == 1){
+        //right and bottom edges
+        if(i-1 >= 0){
+          horizontalLines[i-1][j] = 2;
+        }
+        if(j-1 >= 0){
+          verticalLines[i][j-1] = 2;
+        }
+      }
+      if(i-1 >= 0){
+        if(horizontalLines[i-1][j] == 1 && verticalLines[i][j] == 1){
+          //left and bottom edges
+          horizontalLines[i][j] = 2;
+          if(j-1 >= 0){
+            verticalLines[i][j-1] = 2;
+          }
+        }
+        if(horizontalLines[i-1][j] == 1 && horizontalLines[i][j] == 1){
+          if(j-1 >= 0){
+            verticalLines[i][j-1] = 2;
+          }
+          verticalLines[i][j] = 2;
+        }
+      }
+      if(j-1 >= 0){
+
+      }
+      if(horizontalLines[i][j] == 1 && verticalLines[i-1][j] == 1){
+
+      }
+    }
+  }
+  //go through horizontal lines
+
+  //go through vertical lines
+
+  //go through combinations - use for loop > 5 and a seperate one for edges?
+
+}
+function rule_one(){
+  for(i=0; i < 5; i++){
+    for(j=0; j < 5; j++){
+      if (squares[i][j] == 0){
+        //redundant
+        horizontalLines[i][j] = 2;
+        horizontalLines[i+1][j] = 2;
+        verticalLines[i][j] = 2;
+        verticalLines[i][j+1] = 2;
+      }
+      if(squares[i][j] == remove_x(horizontalLines[i][j]) + remove_x(horizontalLines[i+1][j]) + remove_x(verticalLines[i][j]) + remove_x(verticalLines[i][j+1])){
+        //if square has enough lines, make all 0s Xs(2s) (rule_one)
+        if(horizontalLines[i][j] == 0){
+          horizontalLines[i][j] = 2;
+        }
+        if(horizontalLines[i+1][j] == 0){
+          horizontalLines[i+1][j] = 2;
+        }
+        if(verticalLines[i][j] == 0){
+          verticalLines[i][j] = 2;
+        }
+        if(verticalLines[i][j+1] == 0){
+          horizontalLines[i][j+1] = 2;
+        }
+      }
+    }
+  }
+}
+function rule_two() {
+
+}
+function rule_three() {
+
+}
+function rule_four(){
+
+}
+function rule_five() {
+
+}
+function backtracking_search(){
+
+}
+function mistake_made(){
+  //returns 0 if no mistake made, returns 1 if mistake made
+  for(i=0; i < 5; i++){
+    for(j=0; j < 5; j++){
+      if(squares[i][j] < remove_x(horizontalLines[i][j]) + remove_x(horizontalLines[i+1][j]) + remove_x(verticalLines[i][j]) + remove_x(verticalLines[i][j+1])){
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+function remove_x(num){
+  if(num == 2){
+    return 0;
+  }else{
+    return num;
+  }
+}
+function check_solution(){
+  repeat = true;
+  counter = 0;
+  while (repeat){
+    counter += 1;
+
+  }
+}
+function display_solution(){
+  display_board();
+}
+function display_board(){
+
+}
+
 horizontalLines = [[0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0],
