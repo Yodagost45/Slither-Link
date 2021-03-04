90d0d354035b60b8ab39357650c0842eefd247ae
Yodagost45 <jonathan78564@gmail.com>
2021-03-04 16:23:43 +0000

    Now works with any size of puzzle

--- Slither Link/JS/slitherLink.js
+++ Slither Link/JS/slitherLink.js
@@ -2,12 +2,8 @@ function leftClick(line){
   lineId = line.getAttribute("id");
   idOne = lineId.substring(0,1);
   idTwo = lineId.substring(2,3);
-  if(line.getAttribute("id").includes("horizontal")){
-    orientation = "horizontal";
-  }else{
-    orientation = "vertical"
-  }
-  if(orientation == "horizontal"){
+
+  if(lineId.includes("horizontal")){
     if(horizontalLines[idOne][idTwo] == 1){
       horizontalLines[idOne][idTwo] = 0;
       line.style.backgroundColor = "white";
@@ -29,7 +25,6 @@ function leftClick(line){
     }
   }
 
-  console.log(horizontalLines);
 }
 function rightClick(line){
   line.style.backgroundColor = "white";
@@ -38,21 +33,13 @@ function rightClick(line){
   idOne = lineId.substring(0,1);
   idTwo = lineId.substring(2,3);
 
-  if(line.getAttribute("id").includes("horizontal")){
-    orientation = "horizontal";
-  }else{
-    orientation = "vertical"
-  }
-  console.log(orientation);
-
-  if(orientation == "horizontal"){
+  if(lineId.includes("horizontal")){
     if(horizontalLines[idOne][idTwo] == 2){
       line.style.color = "white";
       horizontalLines[idOne][idTwo] = 0;
     }else{
       line.style.color = "red";
       horizontalLines[idOne][idTwo] = 2;
-      console.log("hi");
     }
   }else{
     if(verticalLines[idOne][idTwo] == 2){
@@ -69,27 +56,104 @@ function displayNum(idOne,idTwo){
   if(squares[idOne][idTwo] == 4){
     return " ";
   }else{
-    console.log(squares[idOne][idTwo]);
     return squares[idOne][idTwo];
   }
 }
-horizontalLines = [[0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0]];
 
-verticalLines = [[0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0]];
+function display_board(){
+  document.getElementById("puzzle-container").innerHTML="";
+  //6.6px = 1%
+  //dotWidth: 3.03030303 100/3% squares.length*4px; 100/squares.length px;
+  //lines: dotWidth*4 px;
+  //squares: dotWidth*4 px;
+  for(i=0; i<squares.length+1; i++){
+    for(j=0; j<squares.length; j++){
+      document.getElementById("puzzle-container").innerHTML+=`<span class="dot">x</span>`;
+      document.getElementById("puzzle-container").innerHTML+=`<a id="${i},${j},horizontal" class="line horizontal-line" onclick="leftClick(document.getElementById('${i},${j},horizontal'))" oncontextmenu="rightClick(document.getElementById('${i},${j},horizontal'))" style="background-color: white;">X</a>`;
+    }
+    document.getElementById("puzzle-container").innerHTML+=`<span class="dot">x</span>`;
+    if(i != squares.length){
+      document.getElementById("puzzle-container").innerHTML+=`<br/>`;
+      for(j=0; j<squares.length+1; j++){
+        document.getElementById("puzzle-container").innerHTML+=`<a id="${i},${j},vertical" class="line vertical-line" onclick="leftClick(document.getElementById('${i},${j},vertical'))" oncontextmenu="rightClick(document.getElementById('${i},${j},vertical'))" style="background-color: white;">X</a>`;
+        if(j != squares.length){
+          document.getElementById("puzzle-container").innerHTML+=`<span id="${i},${j},box" class="num-box">${displayNum(i,j)}</span>`;
+        }
+      }
+      document.getElementById("puzzle-container").innerHTML+=`<br class="after-vertical"/>`;
+    }
+  }
 
-squares = [[4, 3, 4, 1, 4],
+  //changing css
+  dotWidth = 100/squares.length;
+  dots = document.getElementsByClassName('dot');
+  for(i=0; i<dots.length; i++){
+    dots[i].style = `width: ${dotWidth}px; height: ${dotWidth}px; line-height: ${dotWidth}px;`;
+  }
+
+  horizontal_lines = document.getElementsByClassName('horizontal-line');
+  for(i=0; i<horizontal_lines.length; i++){
+    horizontal_lines[i].style = `width: ${dotWidth*5}px; height: ${dotWidth}px; line-height: ${dotWidth}px; font-size: ${dotWidth*0.85}px;`;
+  }
+  vertical_lines = document.getElementsByClassName('vertical-line');
+  for(i=0; i<vertical_lines.length; i++){
+    vertical_lines[i].style = `width: ${dotWidth}px; height: ${dotWidth*5}px; line-height: ${dotWidth*5}px; font-size: ${dotWidth*0.85}px;`;
+  }
+  num_boxes = document.getElementsByClassName('num-box');
+  for(i=0; i<num_boxes.length; i++){
+    num_boxes[i].style = `width: ${dotWidth*5}px; height: ${dotWidth*5}px; line-height: ${dotWidth*5}px; font-size: ${dotWidth*5/2}px;`;
+  }
+  update_board();
+}
+function update_board(){
+  //updates lines filled in based on horizontalLines and verticalLines
+  console.log("hi");
+}
+function initiate_horizontalLines(){
+  //returns horizontalLines based on squares
+  array = [];
+  for(i = 0; i<=squares.length; i++){
+    array.push(Array(squares.length).fill(0));
+  }
+  return array;
+}
+function initiate_verticalLines(){
+  //returns verticalLines based on squares
+  array = [];
+  for(i = 0; i<squares.length; i++){
+    array.push(Array(squares.length+1).fill(0));
+  }
+  return array;
+}
+function initiate_squares(){
+  puzzles = {
+    "0": [[4, 3, 4, 1, 4, 4],
+          [4, 0, 3, 4, 4, 4],
+          [4, 2, 4, 4, 4, 4],
+          [2, 0, 4, 2, 4, 4],
+          [4, 2, 4, 2, 4, 4],
+          [4, 2, 4, 2, 4, 4]],
+    "1": [[4, 3, 4, 1, 4],
           [4, 0, 3, 4, 4],
           [4, 2, 4, 4, 4],
           [2, 0, 4, 2, 4],
-          [4, 2, 4, 2, 4]];
+          [4, 2, 4, 2, 4]],
+    "2": [[4, 3, 4, 1],
+          [4, 0, 3, 4],
+          [4, 2, 4, 4],
+          [2, 0, 4, 2]],
+    "3": [[4, 3, 4],
+          [4, 0, 3],
+          [4, 2, 4]]
+  }
+  return puzzles[Math.floor(Math.random()*Object.keys(puzzles).length)];
+}
+
+
+
+squares = initiate_squares();
+
+horizontalLines = initiate_horizontalLines();
+verticalLines = initiate_verticalLines();
 
 window.oncontextmenu = function(e){ e.preventDefault();}
